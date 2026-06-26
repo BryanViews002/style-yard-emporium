60709885b985fc855603908ef9a2939be0752be9
BryanViews002 <bryanjoe0012@gmail.com>
2026-06-26 16:06:14 -0700

    Update return policy

--- src/pages/Returns.tsx
+++ src/pages/Returns.tsx
@@ -8,22 +8,7 @@ const Returns = () => {
         <section>
           <h2 className="text-2xl text-[--c-ivory] mb-4">Our Return Policy</h2>
           <p>
-            We accept returns within 14 days of delivery. To be eligible for a return, your item must be unused,
-            in the same condition that you received it, and in its original packaging with all tags attached.
-          </p>
-        </section>
-        <section>
-          <h2 className="text-2xl text-[--c-ivory] mb-4">Non-Returnable Items</h2>
-          <p>
-            Certain types of items cannot be returned, including intimate apparel, earrings (for hygiene reasons),
-            custom products, and final sale items.
-          </p>
-        </section>
-        <section>
-          <h2 className="text-2xl text-[--c-ivory] mb-4">Refund Process</h2>
-          <p>
-            Once your return is received and inspected, we will notify you of the approval or rejection of your refund.
-            If approved, a credit will automatically be applied to your original method of payment within 5-7 business days.
+            Clients are only eligible to return a product for a replacement on clothing items. We do not accept returns or exchanges for jewelry.
           </p>
         </section>
       </div>
